3e911495210acec23d7dd5739bd975f3292e9aa1
Koen Klaren <koen.klaren@gmail.com>
2016-05-28 18:59:58 +0200

    Add .editorconfig and apply to core files

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php
-	//create list of samples
+  //create list of samples
   $files = iterator_to_array(
     new RegexIterator(
       new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator("samples")),
       "/\.(wav|mp3|ogg)$/"));
 
-	//sort by time modified when it is within the last two weeks
-	$sortLimit = time() - 14 * 24 * 60 * 60;
+  //sort by time modified when it is within the last two weeks
+  $sortLimit = time() - 14 * 24 * 60 * 60;
 
   usort($files, function($file1, $file2) use ($sortLimit) {
-		$mtime1 = filemtime($file1);
-		$mtime2 = filemtime($file2);
+    $mtime1 = filemtime($file1);
+    $mtime2 = filemtime($file2);
 
-		if ($mtime1 > $sortLimit || $mtime2 > $sortLimit)
-		{
-			return $mtime2 - $mtime1;
-		}
+    if ($mtime1 > $sortLimit || $mtime2 > $sortLimit)
+    {
+      return $mtime2 - $mtime1;
+    }
 
-		return 2 * mt_rand(0, 1) - 1;
-	});
+    return 2 * mt_rand(0, 1) - 1;
+  });
 
   $samples = [];
 
   foreach($files as $file)
   {
-		//windows ¯\_(ツ)_/¯
-		$file = str_replace("\\", "/", $file);
+    //windows ¯\_(ツ)_/¯
+    $file = str_replace("\\", "/", $file);
 
     $pathInfo = pathinfo($file);
     $name = preg_replace("/([^\d])\d{0,2}$/", "\\1", $pathInfo["filename"]);
 
-		//remove first directory from path
-		$dirChain = explode("/", $pathInfo["dirname"]);
-		unset($dirChain[0]);
+    //remove first directory from path
+    $dirChain = explode("/", $pathInfo["dirname"]);
+    unset($dirChain[0]);
     $location = implode("/", $dirChain);
     $file = substr($file, strpos($file, "/") + 1);
 
@@ -42,26 +42,26 @@
 
   $samplesJson = json_encode($samples);
 
-	//create random page title
-	$boardNames = [
-		"music",
-		"spam",
-		"crack",
-		"shit",
-		"originality"
-	];
+  //create random page title
+  $boardNames = [
+    "music",
+    "spam",
+    "crack",
+    "shit",
+    "originality"
+  ];
 
-	$postNames  = [
-		"amirite",
-		"correct",
-		"no",
-		"you see",
-		"eh",
-		"hmm"
-	];
+  $postNames  = [
+    "amirite",
+    "correct",
+    "no",
+    "you see",
+    "eh",
+    "hmm"
+  ];
 
-	$title = "More like " . $boardNames[mt_rand(0, count($boardNames) - 1)] .
-		"board, " . $postNames[mt_rand(0, count($postNames) - 1)]	. "?";
+  $title = "More like " . $boardNames[mt_rand(0, count($boardNames) - 1)] .
+    "board, " . $postNames[mt_rand(0, count($postNames) - 1)]	. "?";
 ?>
 <!-- if this doesn't work you're probably using an inferior browser -->
 <!doctype html>
@@ -72,12 +72,12 @@
     <link rel="stylesheet" href="styles.css" />
     <meta name="viewport" content="initial-scale=1" />
     <link rel="shortcut icon" href="favicon.ico" />
-		<script>
-			//set samples for the script
-			var samples = <?php echo $samplesJson; ?>;
-		</script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" defer></script>
-		<script src="index.js" defer /></script>
+    <script>
+      //set samples for the script
+      var samples = <?php echo $samplesJson; ?>;
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" defer></script>
+    <script src="index.js" defer /></script>
   </head>
 
   <body>
