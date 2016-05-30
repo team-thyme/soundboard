0328ef65e2f5ce17e775cfc35623a4d6b32715b3
Koen Klaren <koen.klaren@gmail.com>
2016-05-30 11:53:57 +0200

    Add font extensions to router.php

--- router.php
+++ router.php
@@ -2,7 +2,7 @@
 
 // Allow files with certain extensions to pass through, all other requests will be internally
 // redirected to index.php.
-if (preg_match('/\.(?:wav|mp3|ogg|js|css|ico)(?:\?.+)?$/', $_SERVER['REQUEST_URI'])) {
+if (preg_match('/\.(?:wav|mp3|ogg|js|css|ico|eot|svg|ttf|woff|woff2)(?:\?.+)?$/', $_SERVER['REQUEST_URI'])) {
   return false;
 } else {
   include __DIR__ . '/public/index.php';
