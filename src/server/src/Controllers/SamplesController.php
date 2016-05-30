c3e93cd552612d17e9bff6d64747c404c4bb43de
Villermen <villermen@gmail.com>
2016-05-31 00:42:08 +0200

    JsonResult, Sample and SamplesController

---
+++ src/server/src/Controllers/SamplesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Villermen\Soundboard\Controllers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Villermen\Soundboard\Models\JsonResult;
+use Villermen\Soundboard\Models\Sample;
+use \RecursiveIteratorIterator;
+use \RegexIterator;
+use \RecursiveDirectoryIterator;
+
+class SamplesController
+{
+	// Todo: injection of these variables from config
+	private $sampleLocation = '../../public/samples';
+	private $publicSampleLocation = 'samples';
+
+	public function listAction(Request $request, Response $response)
+	{
+		$samples = $this->getSamples();
+
+		$response->withJson(new JsonResult([
+			'samples' => $samples
+		]));
+	}
+
+	public function getSamples()
+	{
+		// Get files
+    $iterator = new RecursiveIteratorIterator(
+      new RecursiveDirectoryIterator($this->sampleLocation)
+    );
+
+    // Filter sound files
+    $iterator = new RegexIterator(
+      $iterator,
+      '/\.(wav|mp3|ogg)$/'
+    );
+
+    // Map to sample objects
+    $samples = array_map(function($file) {
+			return new Sample($file);
+		}, iterator_to_array($iterator, false));
+
+		return $samples;
+	}
+}
---
+++ src/server/src/Models/Sample.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Villermen\Soundboard\Models;
+
+use \JsonSerializable;
+
+class Sample implements JsonSerializable
+{
+	// Todo: injection of these variables from config
+	private $sampleLocation = '../../public/samples';
+	private $publicSampleLocation = 'samples';
+
+	private $file;
+
+	private $path;
+	private $name;
+
+	public function __construct($file)
+	{
+		$this->file = $file;
+
+		// Windows compatibility
+		$pathname = str_replace('\\', '/', $this->file->getPathname());
+
+		// Make path relative to public directory
+		$this->path = $this->publicSampleLocation .
+			str_replace($this->sampleLocation, '', $pathname);
+
+		// Conjure a name out of the filename
+		$this->name = preg_replace('/([^\d])\d{0,2}$/', '\1',
+			$file->getBasename('.' . $file->getExtension()));
+	}
+
+	public function jsonSerialize()
+	{
+		return [
+			'file' => $this->path,
+			'name' => $this->name,
+			'id' => hash('crc32', $this->name),
+			'mtime' => $this->file->getMTime()
+		];
+	}
+}
