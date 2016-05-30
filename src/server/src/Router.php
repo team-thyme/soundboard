31dac5e572a4eee9342c699838f54d0b7f76467e
Villermen <villermen@gmail.com>
2016-05-31 00:41:03 +0200

    Router

--- src/server/src/Router.php
+++ src/server/src/Router.php
@@ -8,10 +8,24 @@ class Router
 {
 	private $app;
 	private $config;
+	private $routes;
 
 	public function __construct(App $app)
 	{
 		$this->app = $app;
-		$this->config = $app->getConfig("router");
+		$this->config = $app->getConfig('router');
+
+		$this->configureRoutes();
+	}
+
+	public function configureRoutes()
+	{
+		foreach($this->config['routes'] as $name => $route)
+		{
+			$controller = new $route['controller'];
+
+			//todo: account for methods
+			$this->app->get($route['path'], [ $controller, $route['action'] ]);
+		}
 	}
 }
