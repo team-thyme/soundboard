92a54fbf46550041b0804e62a79f993f89eceae2
Villermen <villermen@gmail.com>
2016-06-03 00:12:15 +0200

    Chain promises better

--- src/client/scripts/ApiClient.js
+++ src/client/scripts/ApiClient.js
@@ -6,26 +6,19 @@ class ApiClient {
   }
 
   getSamples() {
-    return new Promise((resolve) => {
-      fetch(`${this.baseUrl}/samples/list`).then((response) => {
+    return fetch(`${this.baseUrl}/samples/list`)
+      .then((response) => {
         if (response.status === 200) {
-          response.json().then((json) => {
-            const samples = [];
-
-            json.forEach((data) => {
-              // Make the file point back to the used api url
-              data.file = `${this.baseUrl}/samples/get${data.file}`; // eslint-disable-line
-
-              samples.push(new Sample(data));
-            });
-
-            resolve(samples);
-          });
-        } else {
-          throw new Error(`Server replied with ${response.status}`);
+          return response.json();
         }
-      });
-    });
+
+        throw new Error(`Server replied with ${response.status}`);
+      })
+      .then((json) => json.map((data) => {
+        // Make the file point back to the used API url
+        data.file = `${this.baseUrl}/samples/get${data.file}`; // eslint-disable-line
+        return new Sample(data);
+      }));
   }
 }
 
