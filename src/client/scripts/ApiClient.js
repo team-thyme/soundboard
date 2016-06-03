9197cc6151636f87df191586723b22e853e3c706
Villermen <villermen@gmail.com>
2016-06-03 16:40:30 +0200

    Sort everything older than two weeks randomly Déjà vu...

--- src/client/scripts/ApiClient.js
+++ src/client/scripts/ApiClient.js
@@ -15,9 +15,17 @@ class ApiClient {
         throw new Error(`Server replied with ${response.status}`);
       })
       .then((json) => json.map((data) => {
+        /* eslint-disable no-param-reassign */
+
         // Make the file point back to the used API url
-        data.file = `${this.baseUrl}/samples/get${data.file}`; // eslint-disable-line
+        data.file = `${this.baseUrl}/samples/get${data.file}`;
+
+        // Javascript's timestamp uses milliseconds
+        data.mtime = 1000 * data.mtime;
+
         return new Sample(data);
+
+        /* eslint-enable no-param-reassign */
       }));
   }
 }
--- src/client/scripts/main.js
+++ src/client/scripts/main.js
@@ -6,7 +6,15 @@ const $sampleContainer = $('.sample-container');
 const apiClient = new ApiClient();
 
 apiClient.getSamples().then((samples) => {
-  samples.forEach((sample) => {
+  const sortLimit = new Date().getTime() - 14 * 24 * 60 * 60 * 1000;
+
+  samples.sort((sample1, sample2) => {
+    if (sample1.mtime > sortLimit || sample2.mtime > sortLimit) {
+      return sample2.mtime - sample1.mtime;
+    }
+
+    return 2 * Math.floor(2 * Math.random()) - 1;
+  }).forEach((sample) => {
     $sampleContainer.append(sample.$sample);
   });
 });
