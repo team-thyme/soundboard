e42bfde2e8cec63e36b29c9a1671e718d829e073
Koen Klaren <koen.klaren@gmail.com>
2016-06-06 17:24:57 +0200

    Improve default handling in SettingsManager

--- src/client/scripts/helpers/SettingsManager.js
+++ src/client/scripts/helpers/SettingsManager.js
@@ -1,44 +1,42 @@
 import store2 from 'store2';
-import { EventEmitter } from 'events';
+import EventEmitter from 'events';
 
 const STORE_NAMESPACE = 'soundboard';
 
-class SettingsManager {
+const DEFAULT_SETTINGS = {
+  theme: 'default',
+};
+
+class SettingsManager extends EventEmitter {
 
   store;
 
-  eventEmitter;
-
   constructor() {
+    // Init store with namespace
     this.store = store2.namespace(STORE_NAMESPACE);
-    this.eventEmitter = new EventEmitter();
+    // Set default settings, without overwrite
+    this.store.setAll(DEFAULT_SETTINGS, false);
   }
 
   set(key, value, overwrite = true) {
-    this.eventEmitter.emit(key, value);
+    this.emit(key, value);
     return this.store.set(key, value, overwrite);
   }
 
-  get(key, alt) {
-    return this.store.get(key, alt);
+  get(key) {
+    return this.store.get(key);
   }
 
   getAll() {
-    return {
-      theme: this.store.get('theme', 'default'),
-    };
+    return this.store.getAll();
   }
 
   setAll(settings) {
     Object.keys(settings).forEach((key) => {
-      this.eventEmitter.emit(key, settings[key]);
+      this.emit(key, settings[key]);
     });
 
     this.store.setAll(settings);
-  }
-
-  on(key, callback) {
-    this.eventEmitter.on(key, callback);
   }
 
 }
