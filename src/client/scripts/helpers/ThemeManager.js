fe40b7144e5f43cd5509deb9cf97103ddfbe2d4f
Villermen <villermen@gmail.com>
2016-06-12 03:15:37 +0200

    Absolutely horrendous :ok_hand:

--- src/client/scripts/helpers/ThemeManager.js
+++ src/client/scripts/helpers/ThemeManager.js
@@ -4,7 +4,7 @@ import SettingsManager from './SettingsManager';
 export const themes = {
   default: { name: 'Default', primaryColor: '#25b192' },
   classic: { name: 'Classic', primaryColor: '#4caf50' },
-  thyme: { name: 'Thyme', primaryColor: '#00ff00' },
+  thyme: { name: 'Thyme', primaryColor: '#000' },
 };
 
 class ThemeManager {
