49f1ed00bc9d3f00688b47a9e0dbf6e6661651cb
Villermen <villermen@gmail.com>
2016-09-11 20:35:06 +0200

    Base for Telegram API

---
+++ src/server/Controllers/TelegramController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Villermen\Soundboard\Controllers;
+
+use Villermen\Soundboard\Controller;
+
+class TelegramController extends Controller
+{
+	public function webhookAction(Request $request)
+	{
+		
+	}
+}
