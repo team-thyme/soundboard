06e4c5b16f4a6b69deb37d516ed8c96613977d10
Villermen <villermen@gmail.com>
2016-10-27 09:37:36 +0200

    Change directory before converting samples
    Allows running the conversion script from an arbitrary location

--- convert-to-ogg.sh
+++ convert-to-ogg.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cd "${0%/*}"
+
 # Converts .mp3 and .wav samples to ogg
 find samples \( -iname '*.wav' -o -iname '*.mp3' -o -iname '*.flac' \) -exec sh -c 'ffmpeg -i "$1" -codec:a libopus -ac 1 "${1%.*}.ogg"' find-sh {} \; -exec rm {} \;
