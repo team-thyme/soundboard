bdea3bdcafe993f5b672d7189463e767d8a3439d
Villermen <villermen@gmail.com>
2017-05-26 15:20:13 +0200

    Also make gulpfile comply with ESLint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import fs from 'fs-extra';
 import { argv } from 'yargs';
-import { spawn } from 'child_process';
 import path from 'path';
 import del from 'del';
 import sourceStream from 'vinyl-source-stream';
@@ -29,14 +28,14 @@ const distDir = `${__dirname}/dist`;
 const buildDir = `${publicDir}/build`;
 
 const {
-  compress = true,
-  livereload = false,
-  sourcemaps = false,
+    compress = true,
+    livereload = false,
+    sourcemaps = false,
 } = argv;
 
 const browserifyOptions = {
-  entries: ['src/scripts/main.js'],
-  debug: sourcemaps,
+    entries: ['src/scripts/main.js'],
+    debug: sourcemaps,
 };
 
 gulp.task('build', ['build:scripts', 'build:styles', 'build:iconfont']);
@@ -45,102 +44,102 @@ gulp.task('watch', ['watch:scripts', 'watch:styles']);
 gulp.task('clean:styles', () => del([`${buildDir}/*.{css,css.map}`]));
 
 gulp.task('build:styles', ['clean:styles'], () => {
-  const sassOptions = {
-    importer(url, prev, done) {
-      if (url[0] === '~') {
-        url = path.resolve('node_modules', url.substr(1));
-      }
+    const sassOptions = {
+        importer(url) {
+            let resolvedUrl = url;
 
-      return { file: url };
-    },
-  };
+            if (url[0] === '~') {
+                resolvedUrl = path.resolve('node_modules', url.substr(1));
+            }
 
-  const processors = [
-    autoprefixer({ browsers: ['last 1 version'] }),
-  ];
+            return { file: resolvedUrl };
+        },
+    };
 
-  if (compress) {
-    processors.push(cssnano());
-  }
+    const processors = [
+        autoprefixer({ browsers: ['last 1 version'] }),
+    ];
 
-  return gulp.src('src/styles/**/*.scss')
-    .pipe(gulpif(sourcemaps, gulpSourcemaps.init()))
-      .pipe(sass(sassOptions).on('error', sass.logError))
-      .pipe(postcss(processors))
-      .pipe(rename('style.css'))
-    .pipe(gulpif(sourcemaps, gulpSourcemaps.write('./')))
-    .pipe(gulp.dest(buildDir))
-    .pipe(gulpif(livereload, gulpLivereload()));
+    if (compress) {
+        processors.push(cssnano());
+    }
+
+    return gulp.src('src/styles/**/*.scss')
+        .pipe(gulpif(sourcemaps, gulpSourcemaps.init()))
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(postcss(processors))
+        .pipe(rename('style.css'))
+        .pipe(gulpif(sourcemaps, gulpSourcemaps.write('./')))
+        .pipe(gulp.dest(buildDir))
+        .pipe(gulpif(livereload, gulpLivereload()));
 });
 
-gulp.task('watch:styles', (callback) => {
-  if (livereload) {
-    gulpLivereload.listen();
-  }
+gulp.task('watch:styles', () => {
+    if (livereload) {
+        gulpLivereload.listen();
+    }
 
-  gulp.watch('src/styles/**/*.scss', ['build:styles']);
+    gulp.watch('src/styles/**/*.scss', ['build:styles']);
 });
 
 gulp.task('clean:scripts', () => del([`${buildDir}/*.{js,js.map}`]));
 
 function createBundler(options = {}) {
-  return browserify(Object.assign({}, browserifyOptions, options))
-    .transform(babelify)
-    .transform(svgify);
+    return browserify(Object.assign({}, browserifyOptions, options))
+        .transform(babelify)
+        .transform(svgify);
 }
 
 function bundle(bundler) {
-  log('[browserify] Bundle start');
+    log('[browserify] Bundle start');
 
-  return bundler.bundle(() => log('[browserify] Bundle completed'))
-    .on('error', (err) => {
-      log('[browserify]', err.toString());
-    })
-    .pipe(sourceStream('main.js'))
-    .pipe(rename('script.js'))
-    .pipe(buffer())
-    .pipe(gulpif(sourcemaps, gulpSourcemaps.init({ loadMaps: true })))
-      .pipe(gulpif(compress, uglify()))
-    .pipe(gulpif(sourcemaps, gulpSourcemaps.write('./')))
-    .pipe(gulp.dest(buildDir))
-    .pipe(gulpif(livereload, gulpLivereload()));
+    return bundler.bundle(() => log('[browserify] Bundle completed'))
+        .on('error', (err) => {
+            log('[browserify]', err.toString());
+        })
+        .pipe(sourceStream('main.js'))
+        .pipe(rename('script.js'))
+        .pipe(buffer())
+        .pipe(gulpif(sourcemaps, gulpSourcemaps.init({ loadMaps: true })))
+        .pipe(gulpif(compress, uglify()))
+        .pipe(gulpif(sourcemaps, gulpSourcemaps.write('./')))
+        .pipe(gulp.dest(buildDir))
+        .pipe(gulpif(livereload, gulpLivereload()));
 }
 
 gulp.task('build:scripts', ['clean:scripts'], () => {
+    // Copy dist files to public once
+    fs.copy(distDir, publicDir, { clobber: false }, (error) => {
+        if (error && error.code !== 'EEXIST') {
+            throw error;
+        }
+    });
 
-  // Copy dist files to public once
-  fs.copy(distDir, publicDir, { clobber: false }, (error) => {
-    if (error && error.code !== 'EEXIST') {
-      throw error;
-    }
-  });
+    // Below code plain just doesn't work while documentation states it should.
+    // let configCopied = gulp.src('config.dist.json')
+    //   .pipe(rename('config.json'))
+    //   .pipe(gulp.dest(publicDir, { overwrite: false }));
 
-  // Below code plain just doesn't work while documentation states it should.
-  // let configCopied = gulp.src('config.dist.json')
-  //   .pipe(rename('config.json'))
-  //   .pipe(gulp.dest(publicDir, { overwrite: false }));
-
-  return bundle(createBundler());
+    return bundle(createBundler());
 });
 
-gulp.task('watch:scripts', (callback) => {
-  if (livereload) {
-    gulpLivereload.listen();
-  }
+gulp.task('watch:scripts', () => {
+    if (livereload) {
+        gulpLivereload.listen();
+    }
 
-  const bundler = createBundler(watchify.args)
-    .plugin(watchify);
+    const bundler = createBundler(watchify.args)
+        .plugin(watchify);
 
-  bundler.on('update', () => {
-    log('[watch:scripts] Scripts changed, starting build');
+    bundler.on('update', () => {
+        log('[watch:scripts] Scripts changed, starting build');
+        bundle(bundler);
+    });
     bundle(bundler);
-  });
-  bundle(bundler);
 });
 
 gulp.task('clean:iconfont', () => del([`${buildDir}/iconfont/`]));
 
-gulp.task('build:iconfont', ['clean:iconfont'], () => {
-  return gulp.src('src/iconfont/**/*.{css,eot,svg,ttf,woff,woff2}')
-    .pipe(gulp.dest(`${buildDir}/iconfont`));
-});
+gulp.task('build:iconfont', ['clean:iconfont'], () =>
+    gulp.src('src/iconfont/**/*.{css,eot,svg,ttf,woff,woff2}')
+        .pipe(gulp.dest(`${buildDir}/iconfont`)));
