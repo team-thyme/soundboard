1859da8b2ead41259dfeb8e30d70a64f58c8fd70
Villermen <villermen@gmail.com>
2020-07-19 16:12:48 +0200

    Add "watch" mode to build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
 
-if [ "$1" == 'production' ]; then
-    echo 'Building soundboard in "production" mode...'
+case "$1" in
+    prod*) BUILD_MODE='production' ;;
+    dev*) ;&
+    '') BUILD_MODE='development' ;;
+    watch) BUILD_MODE='watch' ;;
+    *) echo "Unknown build mode \"$1\"." && exit 1 ;;
+esac
+
+echo "Building soundboard in \"$BUILD_MODE\" mode..."
+
+if [ "$BUILD_MODE" == 'production' ]; then
     composer install --no-dev --optimize-autoloader
     npm install --production
-else
-    echo 'Building soundboard in "development" mode...'
-    composer install
-    npm install
+    npm run build
+    exit 0
 fi
 
-npm run build
+composer install
+npm install
+
+if [ "$BUILD_MODE" == 'development' ]; then
+    npm run build
+    exit 0
+fi
+
+if [ "$BUILD_MODE" == 'watch' ]; then
+    php -S 0.0.0.0:32658 public/api/index.php & \
+    npm run dev-server & \
+    npm run dev-build
+    exit 0
+fi
--- devserver.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-php -S 0.0.0.0:32658 public/api/index.php & \
-npm run dev-server
