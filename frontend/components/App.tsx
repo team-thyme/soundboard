ad42eb19bc21b0659ed8226562deafb78f05e850
Koen Klaren <koen.klaren@gmail.com>
2020-08-19 18:45:28 +0200

    Sort samples

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -7,7 +7,19 @@ function useSamples(): Sample[] {
     const [samples, setSamples] = useState([]);
 
     useEffect(() => {
-        fetchSamples().then(setSamples);
+        fetchSamples().then((samples) => {
+            // Sort samples
+            const sortLimit = new Date().getTime() - 14 * 24 * 60 * 60 * 1000;
+            samples = samples.sort((sample1, sample2) => {
+                if (sample1.mtime > sortLimit || sample2.mtime > sortLimit) {
+                    return sample2.mtime - sample1.mtime;
+                }
+
+                return 2 * Math.floor(2 * Math.random()) - 1;
+            });
+
+            setSamples(samples);
+        });
     }, []);
 
     return samples;
