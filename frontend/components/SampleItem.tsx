295bd23ff24fc448a3b5985d23a470afd2ea2653
Koen Klaren <koen.klaren@gmail.com>
2020-08-20 21:23:24 +0200

    Add usePlayer hook and playing styles

--- frontend/components/SampleItem.tsx
+++ frontend/components/SampleItem.tsx
@@ -1,20 +1,36 @@
+import { useState, useCallback } from 'react';
 import * as React from 'react';
+import cx from 'classnames';
 import { Sample } from '../api';
-import { player } from "../helpers/Player";
+import { player } from '../helpers/Player';
 
 export interface SampleItemProps {
     sample: Sample;
 }
 
+function usePlayer(sample: Sample): { play(): void; isPlaying: boolean } {
+    // TODO: Get `isPlaying` state from Player instance when component is mounted
+    const [isPlaying, setPlaying] = useState(false);
+
+    const play = useCallback(() => {
+        setPlaying(true);
+        player.play(sample);
+    }, [sample]);
+
+    return { play, isPlaying };
+}
+
 export default function SampleItem({ sample }: SampleItemProps) {
+    const { play, isPlaying } = usePlayer(sample);
+
     return (
         <div
-            className="SampleItem"
+            className={cx('SampleItem', {
+                'SampleItem--isPlaying': isPlaying,
+            })}
             tabIndex={0}
             role="button"
-            onClick={() => {
-                player.play(sample);
-            }}
+            onClick={play}
         >
             <span className="SampleItem__name">{sample.name}</span>
             <span className="SampleItem__detail">
