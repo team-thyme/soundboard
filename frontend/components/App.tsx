c80527a9450596cb06f3084fc86ae28e14fa1a25
Koen Klaren <koen.klaren@gmail.com>
2020-08-20 19:48:01 +0200

    Refactor fetch/filter samples hooks to SampleList.tsx

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,61 +1,7 @@
 import * as React from 'react';
-import { useEffect, useMemo, useState } from 'react';
-import { fetchSamples, Sample } from '../api';
+import { useMemo, useState } from 'react';
 import Header from './Header';
 import SampleList from './SampleList';
-
-function useSamples(): Sample[] {
-    const [samples, setSamples] = useState([]);
-
-    useEffect(() => {
-        fetchSamples().then((samples) => {
-            // Sort samples
-            const sortLimit = new Date().getTime() - 14 * 24 * 60 * 60 * 1000;
-            samples = samples.sort((sample1, sample2) => {
-                if (sample1.mtime > sortLimit || sample2.mtime > sortLimit) {
-                    return sample2.mtime - sample1.mtime;
-                }
-
-                return 2 * Math.floor(2 * Math.random()) - 1;
-            });
-
-            setSamples(samples);
-        });
-    }, []);
-
-    return samples;
-}
-
-function useFilteredSamples(query: string): Sample[] {
-    const samples = useSamples();
-    return useMemo(() => {
-        if (query.trim() === '') {
-            return samples;
-        }
-
-        // Prepare regex
-        const terms = query
-            // Strip non-alphanumeric characters (will be done in target as well)
-            .replace(/[^\w\s|]/g, '')
-            // Enable OR-searching when whitespace is around the pipe character "|"
-            .replace(/\s+\|\s+/g, '|')
-            // Split by any combination of whitespace characters
-            .split(/[\s+&]+/g);
-        const regex = new RegExp(
-            `.*${terms.map((term) => `(?=.*${term}.*)`).join('')}.*`,
-            'i',
-        );
-
-        return samples.filter((sample) => {
-            let filterString = sample.name.replace(/[^\w\s|]/g, '');
-            sample.categories.forEach((category) => {
-                filterString += ' ' + category.replace(/[^\w\s|]/g, '');
-            });
-
-            return regex.test(filterString);
-        });
-    }, [samples, query]);
-}
 
 export const SearchContext = React.createContext({
     query: '',
@@ -64,8 +10,6 @@ export const SearchContext = React.createContext({
 
 export default function App() {
     const [query, setQuery] = useState('');
-    const samples = useFilteredSamples(query);
-
     const searchContext = useMemo(() => ({ query, setQuery }), [
         query,
         setQuery,
@@ -74,7 +18,7 @@ export default function App() {
     return (
         <SearchContext.Provider value={searchContext}>
             <Header />
-            <SampleList samples={samples} />
+            <SampleList />
         </SearchContext.Provider>
     );
 }
--- frontend/components/SampleList.tsx
+++ frontend/components/SampleList.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState, useContext } from 'react';
 import { List, WindowScroller } from 'react-virtualized';
 
-import { Sample } from '../api';
+import { fetchSamples, Sample } from "../api";
 import { useTextMeasurer } from '../helpers/TextMeasurer';
 import {
     detailFont,
@@ -12,12 +12,70 @@ import {
     sampleMargin,
     samplePaddingX,
 } from '../styles/sync-variables';
+import { SearchContext } from "./App";
 import SampleItem from './SampleItem';
 
-interface SampleListProps {
-    samples: Sample[];
+/**
+ * React hook. Fetches samples from API, if needed.
+ */
+function useSamples(): Sample[] {
+    const [samples, setSamples] = useState([]);
+
+    useEffect(() => {
+        fetchSamples().then((samples) => {
+            // Sort samples
+            const sortLimit = new Date().getTime() - 14 * 24 * 60 * 60 * 1000;
+            samples = samples.sort((sample1, sample2) => {
+                if (sample1.mtime > sortLimit || sample2.mtime > sortLimit) {
+                    return sample2.mtime - sample1.mtime;
+                }
+
+                return 2 * Math.floor(2 * Math.random()) - 1;
+            });
+
+            setSamples(samples);
+        });
+    }, []);
+
+    return samples;
 }
 
+/**
+ * React hook. Filters a list of samples using the given query.
+ */
+function useFilteredSamples(samples: Sample[], query: string): Sample[] {
+    return useMemo(() => {
+        if (query.trim() === '') {
+            return samples;
+        }
+
+        // Prepare regex
+        const terms = query
+            // Strip non-alphanumeric characters (will be done in target as well)
+            .replace(/[^\w\s|]/g, '')
+            // Enable OR-searching when whitespace is around the pipe character "|"
+            .replace(/\s+\|\s+/g, '|')
+            // Split by any combination of whitespace characters
+            .split(/[\s+&]+/g);
+        const regex = new RegExp(
+            `.*${terms.map((term) => `(?=.*${term}.*)`).join('')}.*`,
+            'i',
+        );
+
+        return samples.filter((sample) => {
+            let filterString = sample.name.replace(/[^\w\s|]/g, '');
+            sample.categories.forEach((category) => {
+                filterString += ' ' + category.replace(/[^\w\s|]/g, '');
+            });
+
+            return regex.test(filterString);
+        });
+    }, [samples, query]);
+}
+
+/**
+ * React hook. Measures the rendered width of each sample in the given list of samples.
+ */
 function useSampleWidths(samples: Sample[]): number[] {
     const nameMeasurer = useTextMeasurer(nameFont);
     const detailMeasurer = useTextMeasurer(detailFont);
@@ -53,6 +111,9 @@ function computeLayout(itemWidths: number[], maxRowWidth: number): number[][] {
     return rows;
 }
 
+/**
+ * React hook. Measures the width of the body element.
+ */
 function useBodyWidth(): number {
     const [width, setWidth] = useState(document.body.clientWidth);
 
@@ -67,7 +128,11 @@ function useBodyWidth(): number {
     return width;
 }
 
-export default function SampleList({ samples }: SampleListProps) {
+export default function SampleList() {
+    const allSamples = useSamples();
+    const { query } = useContext(SearchContext);
+    const samples = useFilteredSamples(allSamples, query);
+
     const containerWidth = useBodyWidth() - sampleListPadding * 2;
     const rowHeight = sampleHeight + sampleMargin * 2;
 
