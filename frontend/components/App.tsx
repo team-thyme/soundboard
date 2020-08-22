a8f1d6a875c725589a684c0f3efba4cb57418ed3
Koen Klaren <koen.klaren@gmail.com>
2020-08-22 11:19:37 +0200

    Add 'Space' shortcut for stopping all playing samples

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { useMemo, useState, Context } from 'react';
+import { useMemo, useState, Context, useEffect } from 'react';
 import Header from './Header';
 import SampleList from './SampleList';
+import { player } from '../helpers/Player';
 
 export const SearchContext: Context<{
     query: string;
@@ -9,11 +10,33 @@ export const SearchContext: Context<{
 }> = React.createContext(null);
 
 export default function App() {
+    // Search context
     const [query, setQuery] = useState('');
     const searchContext = useMemo(() => ({ query, setQuery }), [
         query,
         setQuery,
     ]);
+
+    // Handle keyboard shortcuts
+    useEffect(() => {
+        function handleKeyDown(e) {
+            if (e.target.tagName === 'INPUT') {
+                return;
+            }
+
+            switch (e.code) {
+                case 'Space':
+                    player.stopAll();
+                    e.preventDefault();
+                    break;
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    });
 
     return (
         <SearchContext.Provider value={searchContext}>
