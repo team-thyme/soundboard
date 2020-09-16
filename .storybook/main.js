8860463c06f2b206577be8232d41d1145538dbaf
Koen Klaren <koen.klaren@gmail.com>
2020-09-16 13:42:17 +0200

    Begin adding Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../frontend/stories/**/*.stories.mdx",
+    "../frontend/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-scss",
+    // "@storybook/preset-typescript",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+import '../frontend/styles/styles.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
