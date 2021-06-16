ab0b7c15ff9b3afb1bb6181fb3e4f7d4eb08e574
Koen Klaren <koen.klaren@gmail.com>
2021-06-16 21:30:39 +0200

    Optimize imports

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { usePopper } from 'react-popper';
 import useKeydown from '../hooks/useKeydown';
