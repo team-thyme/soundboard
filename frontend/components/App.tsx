af6013eacab45a33c82e6f4261ea0f2e08003d14
Koen Klaren <koen.klaren@gmail.com>
2021-06-16 18:24:52 +0200

    Extract search context hooks into a separate hook

--- frontend/components/App.tsx
+++ frontend/components/App.tsx
@@ -3,23 +3,25 @@ import React, { Context, useMemo, useState } from 'react';
 import Header from './Header';
 import SampleList from './SampleList';
 
-export const SearchContext: Context<{
+type SearchContextValue = {
     query: string;
     setQuery(query: string): void;
-}> = React.createContext(
+};
+
+export const SearchContext: Context<SearchContextValue> = React.createContext(
     // This will cause errors if no SearchContext.Provider is used, but that
     // should never happen. And I would prefer a hard error over it silently not
     // working any day.
     undefined as any,
 );
 
-export default function App() {
-    // Search context
+function useSearchContextValue(): SearchContextValue {
     const [query, setQuery] = useState('');
-    const searchContext = useMemo(
-        () => ({ query, setQuery }),
-        [query, setQuery],
-    );
+    return useMemo(() => ({ query, setQuery }), [query, setQuery]);
+}
+
+export default function App() {
+    const searchContext = useSearchContextValue();
 
     return (
         <SearchContext.Provider value={searchContext}>
