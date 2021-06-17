7b273f7a091b118380a1d061321fa134d2c6bbe9
Koen Klaren <koen.klaren@gmail.com>
2021-06-17 21:38:29 +0200

    Start adding theme thumbs

--- frontend/components/ThemeSelect.tsx
+++ frontend/components/ThemeSelect.tsx
@@ -19,9 +19,60 @@ export default function ThemeSelect(): JSX.Element {
                             }}
                         />
                         {key}
+                        <ThemeThumb theme={value} />
                     </label>
                 </div>
             ))}
         </div>
+    );
+}
+
+interface ThemeThumbProps {
+    theme: Theme;
+}
+
+function ThemeThumb({ theme }: ThemeThumbProps): JSX.Element {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="ThemeThumb"
+            data-theme={theme}
+            width={120}
+            height={90}
+            viewBox="0 0 120 90"
+        >
+            {/* Header */}
+            <rect className="ThemeThumb__header" width={120} height={22} />
+            <text
+                className="ThemeThumb__headerTitle"
+                fontSize={10}
+                x={5}
+                y={15}
+                textAnchor="start"
+            >
+                Sb
+            </text>
+            <rect
+                className="ThemeThumb__search"
+                x={30}
+                y={4}
+                width={60}
+                height={14}
+                rx={2}
+                ry={2}
+            />
+            <circle className="ThemeThumb__headerIcon" cx={110} cy={11} r={2} />
+            <circle className="ThemeThumb__headerIcon" cx={104} cy={11} r={2} />
+
+            {/* Samples */}
+            <rect
+                className="ThemeThumb__sampleList"
+                y={22}
+                width={120}
+                height={68}
+            />
+
+            {/* TODO: Add samples */}
+        </svg>
     );
 }
