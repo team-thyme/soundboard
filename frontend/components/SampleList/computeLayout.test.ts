04b997278ff8663478ffb18d7f9f500fcbe7f178
Koen Klaren <koen.klaren@gmail.com>
2021-06-21 21:46:15 +0200

    Add more computeLayout tests

--- frontend/components/SampleList/computeLayout.test.ts
+++ frontend/components/SampleList/computeLayout.test.ts
@@ -1,5 +1,9 @@
 import computeLayout from './computeLayout';
 
+test('no items', () => {
+    expect(computeLayout([], 3)).toStrictEqual([]);
+});
+
 test('two items on a single row', () => {
     expect(computeLayout([1, 1], 3)).toStrictEqual([[0, 1]]);
 });
@@ -10,4 +14,12 @@ test('three items exactly filling a single row', () => {
 
 test('four items overflowing a single row', () => {
     expect(computeLayout([1, 1, 1, 1], 3)).toStrictEqual([[0, 1, 2], [3]]);
+});
+
+test('single item larger than max row width', () => {
+    expect(computeLayout([4], 3)).toStrictEqual([[0]]);
+});
+
+test('second item larger than max row width', () => {
+    expect(computeLayout([1, 4], 3)).toStrictEqual([[0], [1]]);
 });
