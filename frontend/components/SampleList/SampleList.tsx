4e3d08a9d58fa3f6d43d79987ed76b371bddbffd
Koen Klaren <koen.klaren@gmail.com>
2021-06-21 21:15:18 +0200

    Move computeLayout to separate file

--- frontend/components/SampleList/SampleList.tsx
+++ frontend/components/SampleList/SampleList.tsx
@@ -20,6 +20,7 @@ import {
 } from '../../styles/sync-variables';
 import { SearchContext } from '../App';
 import SampleItem from '../SampleItem/SampleItem';
+import computeLayout from './computeLayout';
 
 /**
  * React hook. Fetches samples from API, if needed.
@@ -109,26 +110,6 @@ function useSampleWidths(samples: Sample[]): number[] {
     );
 
     return useMemo(() => samples.map(getWidth), [samples, getWidth]);
-}
-
-function computeLayout(itemWidths: number[], maxRowWidth: number): number[][] {
-    if (itemWidths.length === 0) {
-        return [];
-    }
-
-    // TODO: Better layout computation. Perhaps do local search (with badness like LaTeX) after the current greedy approach.
-    let rows: number[][] = [[]];
-    let rowWidth = 0;
-    for (let itemIndex = 0; itemIndex < itemWidths.length; ++itemIndex) {
-        if (rowWidth === 0 || rowWidth + itemWidths[itemIndex] < maxRowWidth) {
-            rowWidth += itemWidths[itemIndex];
-            rows[rows.length - 1].push(itemIndex);
-        } else {
-            rowWidth = itemWidths[itemIndex];
-            rows.push([itemIndex]);
-        }
-    }
-    return rows;
 }
 
 /**
---
+++ frontend/components/SampleList/computeLayout.ts
@@ -0,0 +1,22 @@
+export default function computeLayout(
+    itemWidths: number[],
+    maxRowWidth: number,
+): number[][] {
+    if (itemWidths.length === 0) {
+        return [];
+    }
+
+    // TODO: Better layout computation. Perhaps do local search (with badness like LaTeX) after the current greedy approach.
+    let rows: number[][] = [[]];
+    let rowWidth = 0;
+    for (let itemIndex = 0; itemIndex < itemWidths.length; ++itemIndex) {
+        if (rowWidth === 0 || rowWidth + itemWidths[itemIndex] < maxRowWidth) {
+            rowWidth += itemWidths[itemIndex];
+            rows[rows.length - 1].push(itemIndex);
+        } else {
+            rowWidth = itemWidths[itemIndex];
+            rows.push([itemIndex]);
+        }
+    }
+    return rows;
+}
