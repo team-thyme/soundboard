321f0d1ba2a1d84168bc85ea1fbd290b2f2261b2
Koen Klaren <koen.klaren@gmail.com>
2021-11-02 21:34:09 +0100

    Add arrow key navigation through samples list

--- frontend/components/SampleItem/SampleItem.tsx
+++ frontend/components/SampleItem/SampleItem.tsx
@@ -65,6 +65,80 @@ function usePlayer(sample: Sample): {
     return { togglePlay, isPlaying, progresses, analyserNode };
 }
 
+// TODO: Move sample navigation code to SampleList
+
+type ArrowKey = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight';
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+const keyToDirection: Record<ArrowKey, Direction> = {
+    ArrowUp: 'up',
+    ArrowDown: 'down',
+    ArrowLeft: 'left',
+    ArrowRight: 'right',
+};
+
+function navigateSample(target: HTMLElement, direction: Direction) {
+    let nextTarget = null;
+
+    switch (direction) {
+        case 'left':
+            nextTarget = target.previousElementSibling;
+            break;
+        case 'right':
+            nextTarget = target.nextElementSibling;
+            break;
+        case 'up':
+            nextTarget = findClosest(
+                target,
+                target.parentElement?.previousElementSibling,
+            );
+            break;
+        case 'down':
+            nextTarget = findClosest(
+                target,
+                target.parentElement?.nextElementSibling,
+            );
+            break;
+    }
+
+    if (nextTarget instanceof HTMLElement) {
+        nextTarget.focus();
+    }
+}
+
+function findClosest(
+    target: Element,
+    row: Element | undefined | null,
+): Element | null {
+    if (row === null || row === undefined) {
+        return null;
+    }
+
+    const { left: targetMin, right: targetMax } =
+        target.getBoundingClientRect();
+    const targetMean = (targetMin + targetMax) / 2;
+
+    let closest = null;
+    let closestDissimilarity = Infinity;
+
+    Array.from(row.children).forEach((child) => {
+        const { left: childMin, right: childMax } =
+            child.getBoundingClientRect();
+        const childMean = (childMin + childMax) / 2;
+
+        const dissimilarity =
+            Math.abs(targetMean - childMean) /
+            (Math.max(targetMax, childMax) - Math.min(targetMin, childMin));
+
+        if (dissimilarity < closestDissimilarity) {
+            closest = child;
+            closestDissimilarity = dissimilarity;
+        }
+    });
+
+    return closest;
+}
+
 export default function SampleItem({ sample }: SampleItemProps) {
     const { togglePlay, isPlaying, progresses, analyserNode } =
         usePlayer(sample);
@@ -127,6 +201,19 @@ export default function SampleItem({ sample }: SampleItemProps) {
                         });
                         e.preventDefault();
                         e.stopPropagation();
+                    }}
+                    onKeyDown={(e) => {
+                        const { key } = e;
+
+                        if (key in keyToDirection) {
+                            e.preventDefault();
+                            navigateSample(
+                                e.target as HTMLElement,
+                                keyToDirection[
+                                    key as keyof typeof keyToDirection
+                                ],
+                            );
+                        }
                     }}
                     {...props}
                 >
