c1ec82155e15db580d9304b350239d28f34db87b
Villermen <villermen@gmail.com>
2022-10-24 08:13:15 +0200

    Ogg better. Convert to ogg

--- convert-samples.sh
+++ convert-samples.sh
@@ -2,5 +2,5 @@
 
 cd "${0%/*}"
 
-# Converts .mp3, .ogg and .wav samples to .webm opus.
-find samples \( -iname '*.wav' -o -iname '*.mp3' -o -iname '*.flac' -o -iname '*.ogg' \) -exec sh -c 'ffmpeg -i "$1" -map_metadata -1 -codec:a libopus -ac 1 "${1%.*}.webm"' find-sh {} \; -exec rm {} \;
+# Converts .mp3, .webm and .wav samples to .ogg opus.
+find samples \( -iname '*.wav' -o -iname '*.mp3' -o -iname '*.flac' -o -iname '*.webm' \) -exec sh -c 'ffmpeg -i "$1" -map_metadata -1 -codec:a libopus -ac 1 "${1%.*}.ogg"' find-sh {} \; -exec rm {} \;
