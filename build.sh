15d65aec9a732d6b165769d8f26ba356cfd51586
Villermen <villermen@gmail.com>
2022-11-13 14:23:01 +0100

    Use Yarn in build.sh and package scripts
    Because Yarn is better, I guess?

--- build.sh
+++ build.sh
@@ -14,21 +14,21 @@ echo "Building soundboard in \"$BUILD_MODE\" mode..."
 if [ "$BUILD_MODE" == 'production' ]; then
     test -f .env && source .env
     composer install --no-dev --optimize-autoloader
-    npm install
-    BASE_URL=${BASE_URL:=/} npm run build-production
+    yarn install
+    BASE_URL=${BASE_URL:=/} yarn run build-production
     exit 0
 fi
 
 composer install
-npm install
+yarn install
 
 if [ "$BUILD_MODE" == 'development' ]; then
-    npm run build
+    yarn run build
     exit 0
 fi
 
 if [ "$BUILD_MODE" == 'serve' ]; then
     composer run dev-server & \
-    npm run dev-server
+    yarn run dev-server
     exit 0
 fi
